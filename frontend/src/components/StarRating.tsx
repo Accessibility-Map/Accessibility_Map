22b4363934b33635cd4e37aea6fc05c34f28dbad
Jonathan Kim <kim6jk@mail.uc.edu>
2024-11-07 15:48:55 -0500

    Fix star ratings and add Rating service

--- frontend/src/components/StarRating.tsx
+++ frontend/src/components/StarRating.tsx
@@ -1,15 +1,41 @@
-import React, { useState } from 'react';
+import React, { useEffect, useState } from 'react';
+import RatingService from './services/RatingService'
+import './styles/StarRating.css';
 
-const StarRating = ({ locationID }: { locationID: number }) => {
+interface StarRatingProps{
+  locationID: number
+}
+
+const StarRating = ({ locationID }: StarRatingProps) => {
   const [hover, setHover] = useState(0); // Hovered star
   const [currentRating, setCurrentRating] = useState(0); // Set default rating to 0
   const [unset, setUnset] = useState(false); // never been set in db flag
+
+  useEffect(() => {
+    let currentRating = RatingService.getRating(1, locationID).then((rating) => {
+      if (rating && rating.getRating() !== 0) {
+        setCurrentRating(rating.getRating());
+      }else{
+        setUnset(true);
+      }
+    });
+  });
 
   // Function to update rating in the state
   const updateRating = (rating: number) => {
     if (rating > 5 || rating < 1) {
       return;
     }
+
+        // Update the current rating and backend API
+      if (unset) {
+        setCurrentRating(0);  // Properly update state with useState's function
+        RatingService.createRating(1, locationID, rating);  // Backend call to create a rating
+        setUnset(false);
+      } else {
+        setCurrentRating(rating);  // Properly update state with useState's function
+        RatingService.setRating(1, locationID, rating);   // Backend call to set a rating
+      }
 
     // Update the current rating state
     setCurrentRating(rating);
